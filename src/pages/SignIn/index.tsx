3f23bd4abde0c47e8c70a60c29f46b9add27f25e
Caio Yoshida Dell G3 <caio.yoshida@yahoo.com.br>
2021-02-24 16:51:14 -0300

    chore: building SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,64 @@
 import React from 'react';
-import { Image } from 'react-native';
-
-import { Container, Title } from './styles';
+import {
+  Image, KeyboardAvoidingView, Platform, View, ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
-    <Title>Enter with your login</Title>
-  </Container>
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
+const SignIn: React.FC = () => (
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Enter with your login</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Password" />
+
+          <Button onPress={() => {}}>
+            Enter
+          </Button>
+
+          <ForgotPassword onPress={() => {}}>
+            <ForgotPasswordText>
+              Forgot my password
+            </ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <CreateAccountButton onPress={() => {}}>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>
+        Create new account
+      </CreateAccountButtonText>
+    </CreateAccountButton>
+  </>
 );
 
 export default SignIn;
